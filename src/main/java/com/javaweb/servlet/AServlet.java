18f4bcab4f26193036b138401668c851fb55b84f
fank <fank@aaa.com>
2020-06-03 15:42:01 +0800

    aaaa

---
+++ src/main/java/com/javaweb/servlet/AServlet.java
@@ -0,0 +1,23 @@
+package com.javaweb.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("aSerlvet");
+		response.getWriter().print("ok");
+	}
+
+}
